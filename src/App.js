b6bcbced141936f4447da5468f552cb9a16f1347
ArsenGabrielyan <arsentech5002@gmail.com>
2022-09-08 18:37:43 +0400

    Avelacrel em ReactJS Project

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Սա սովորական React.JS Պրոյեկտ է
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
