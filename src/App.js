6486944128ac327c54cb5c755465c3bbd80082b3
ArsenGabrielyan <arsentech5002@gmail.com>
2022-09-09 16:22:26 +0400

    Added Menu

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <div className="menu">
         <div className="buttons">
-          <button><Icon icon="carbon:add-filled" /> Add to do</button>
-          <button><Icon icon="carbon:add-filled" /> Add to do</button>
-          <button><Icon icon="carbon:add-filled" /> Add to do</button>
+          <button>Add to do</button>
+            <span className="pink btn1"><Icon icon="carbon:add-filled" /></span>
+          <button className="active">To do list</button>
+            <span className="pink btn2"><Icon icon="bi:card-list" /></span>
+          <button> Completed</button>
+            <span className="pink btn3"><Icon icon="bi:list-check" /></span>
         </div>
       </div>
       <div className="container"></div>
